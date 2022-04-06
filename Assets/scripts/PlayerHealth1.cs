8358685817beb12147efe4f65aa08248ac4f7096
PuraCaseus <94827270+PuraCaseus@users.noreply.github.com>
2022-04-06 14:10:25 +0200

    Game is now mario kart

---
+++ Assets/scripts/PlayerHealth1.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.Resources;
+using UnityEngine;
+
+public class PlayerHealth1 : MonoBehaviour
+{
+    public int health;
+    public int numberOfHearts;
+    public GameObject[] heartsOfGameObjects;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+
+    private List<SpriteRenderer> ListHeartsSRen;
+    private List<Sprite> ListSpriteHearts;
+
+    private int HeartLastIndex;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        PlayerDeath();
+
+    }
+
+    public void DamageToPlayer()
+    {
+        print("Got hit Player");
+        health--;
+    }
+    void PlayerDeath()
+    {
+        if(health <= 0)
+        {
+            print("Killed");
+            //Future: Return to players base
+            Destroy(gameObject);
+        }
+    }
+
+}
